2b11c123607e29515e67597b1a433772a38b368a
Okennwa2022 <adindumichael@yahoo.com>
2022-06-30 17:25:03 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ *  main - entry point
+ *  
+ *  return: 0 after printing function
+ */
 
-int main(void)"{"
-puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+char "var" ("\"Programming is like building a multilingual puzzle");
+
+puts(var)
 return (0);
 }
