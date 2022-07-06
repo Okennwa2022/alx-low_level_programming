ec7e802e192a29d29d2e515ff7d0526f5280b3e2
Okennwa2022 <adindumichael@yahoo.com>
2022-07-05 18:15:41 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* _isalpha - checks or alphabetical letter
-* @c: a charter to be checked on
+* test_isalpha - Test the _isalpha function
+* @c: Number to pass to _isalpha function
 * Return: returns 0 or 1 depending on condition
 */
 int _islower(int c)
