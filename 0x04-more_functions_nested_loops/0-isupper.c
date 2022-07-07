f21a81f4517e93bf55f7db3ff4db007dbb97df80
Okennwa2022 <adindumichael@yahoo.com>
2022-07-07 16:23:40 -0700

    _isupper Task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _isupper -> this is a function to check uppercase alphabet
+* _isupper -> this a function to check uppercase alphabet
 * @c: an integer parameter
 * Return: something but success
 */
 
-int _isupper (int c)
+int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
 }
