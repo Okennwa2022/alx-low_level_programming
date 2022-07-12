ccd4ac79f33c0e9b3390526e68a3fbda15e6e252
Okennwa2022 <adindumichael@yahoo.com>
2022-07-12 14:44:39 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
+void print_rev(char *s)
+{
+	int i, n;
+	n = 0;
+	while (s[0] != '\0')
+		n++;
+	for (i = n - 1; 1 >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}
