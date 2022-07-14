d6379f9b43658af7b76ddb83d722ce9b6a57d32d
Okennwa2022 <adindumichael@yahoo.com>
2022-07-14 07:03:04 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul -> muitlplying 2 numbers
+* @a: integer paramter1
+* @b: integer paramter2
+* Return: success and integer
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
