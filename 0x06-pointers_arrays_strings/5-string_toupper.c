6370211fc94d3b7b761c8f17fef698e39bab2df2
Okennwa2022 <adindumichael@yahoo.com>
2022-07-14 04:56:00 -0700

    tupper added

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,13 @@
 
 char *string_tupper(char *x)
 {
-	int i = 0
+	int i = 0;
 
 		while (x[i])
 		{
-			if (x[i]) >= 97 && x[i] <= 122)
+			if (x[i] >= 97 && x[i]  <= 122)
 				x[i] = x[i] - 32;
 			i++;
 		}
+		return (x);
 }
