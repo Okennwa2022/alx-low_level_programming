1b883b28dc98da658d13d274255d1362aaefd81a
Okennwa2022 <adindumichael@yahoo.com>
2022-07-14 18:43:45 -0700

    prints

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,16 +3,14 @@
 /**
 * main -> assign a random number to the variable n each time it is executed
 * and print the last digit of the number stored in the variable n
-* Return: Always 0 (Success)
+* Return: Always 0 (success)
 */
 int main(void)
 {
-int n;
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{	putchar(ch);
+}
+ putchar (10); /* this is an ascii code for new line*/
 
-for (n = 0; n < 10; n++)
-{
-printf("%d", n);
-}
-printf("\n");
-return (0);
-}
+ return (0);
