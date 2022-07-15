5b6db8b88da56bae5c9e26fa0f5c0cb6bdb0fd2f
Okennwa2022 <adindumichael@yahoo.com>
2022-07-15 15:55:24 -0700

    print isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
 * @c: Number to pass to _isalpha function
 * Return: returns 0 or 1 depending on condition
 */
-int _islower(int c)
+int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
