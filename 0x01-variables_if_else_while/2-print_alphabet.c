7a7acc720568ab76703af2b6d4dfbd89cf7fcdc2
Okennwa2022 <adindumichael@yahoo.com>
2022-07-14 18:19:18 -0700

    print alpabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	int ch;
-	for(ch = 97; ch <= 122; ch++)
+	
+	for (ch = 97; ch <= 122; ch++)
 	{
 		putchar(ch);
 	}	
 	putchar(10); /* this is an ascii code for a new line */
 
-return (0);
+	return (0);
 }
