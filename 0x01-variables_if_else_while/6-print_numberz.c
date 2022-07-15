cc8edbb7ec16206fc209372ec2892aadc44aba36
Okennwa2022 <adindumichael@yahoo.com>
2022-07-15 14:41:57 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#include <stdio.h>
 
-/**                                                                                                                                           
- *  main -> program that prints the alphabet in lowercase                                                                                      
- *  Return : always 0                                                                                                                          
- *  */          
-
-int main(void)                                                                                                                                
+/**
+* main -> assign a random number to the variable n each time it is executed
+* and print the last digit of the number stored in the variable n
+* Return: Always 0(Success)
+*/
+int main(void)
 {
-	int n;
-	for (n = 0; n < 10; n==)
-	{
-		printf("%d", n);
-	}
-	printf("\n");          
-	return (0);
-}   
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+
+putchar(ch);
+}
+
+putchar(10); /* this is a ascii code for new lines*/
+
+return (0);
+}
