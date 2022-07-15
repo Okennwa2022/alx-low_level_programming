57b4493bcb43d384e372fd62d75b80a5bee4372c
Okennwa2022 <adindumichael@yahoo.com>
2022-07-15 15:37:59 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
+#include <stdio.h>
 
-/**                                                                                                                                           
- *  main -> program that prints all possible combinations of single-digit numbers                                                                                     
- *  Return : always 0                                                                                                                          
- */          
-int main(void)                                                                                                                                
-{             
-	int ch;
-	for (ch = 48; ch <= 57; ch++)
-	{
+/**
+* main -> assign a random number to the variable n each time it is executed
+* and print the lastdigit of the number stored in the variable n
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
 
-		putchar(ch);
-		if (ch != 57)
-		{
-			putchar(44);
-			putchar(32);
-		}
-	}
+putchar(ch);
+if (ch != 57)
+{
+putchar(44);
+putchar(32);
+{
 
-	putchar(10); /* this is an ascii code for a new line */
+putchar(10); /* this is an ascii code for new line*/
 
-	return (0);
+return (0);
 }
-
+}
+}
+}
