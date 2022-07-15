9fbdc3a664a9a95577e76524e746e30a3786f753
Okennwa2022 <adindumichael@yahoo.com>
2022-07-15 15:13:19 -0700

    print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,31 +1,26 @@
-#include <stdio.h>
+#include <stdio.h>
 
-/**                                                                                                                                           
- *  * main -> program that prints the alphabet in lowercase                                                                                      
- *  * Return : always 0                                                                                                                          
- *  */          
-int main(void)                                                                                                                                
-{             
-	/**
-	 * hexadecimal
-	 * 0 1 2 3 4 5 6 7 8 9 a b c d e f
-	 */
-	char ch;
-	int n;
-	for (n = 48; n <= 57; n++)
-	{
-		putchar(n);
+/**
+* main -> assign a random number to the variable n each time it is executed
+* and print the last digit of the number stored in the variable n
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+int n;
 
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
-	}
+for (n = 48; n <= 57; n++)
+{
 
-	putchar(10);
-
-	return (0);
+putchar(n);
 }
-	 
+for (ch = 'a'; ch <= 'f'; ch++)
+{
 
+putchar(ch);
+}
+putchar(10);
 
+return (0);
+}
