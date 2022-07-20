b83efbcbc2c40ad2be2a15595ca223aae6a45380
Okennwa2022 <adindumichael@yahoo.com>
2022-07-20 16:21:22 -0700

    prime number added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int_sqrt(int, Int);
+
+/**
+* _sqrt_recursion -> sqrt using recursion
+* @n: parameter to be sqrt
+* Return: square root of a number
+*/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+* _sqrt -> recursive square root
+* @n: number
+* @i: iterator
+* Return: a number
+*/
+int _sqrt(int n, int 1)
+{
+	int square = i * i;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
