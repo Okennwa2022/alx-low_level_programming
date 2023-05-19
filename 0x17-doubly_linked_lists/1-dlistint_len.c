61c27433ce7b682cbb5c6a49342e3e3eb7133a4e
root <adindumichael@yahoo.com>
2023-05-18 20:07:05 -0700

    first commit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints data in nodes
+ * dlistint_len - counts number of nodes
  * @h: head of node
- * Return: how many nodes
+ * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
